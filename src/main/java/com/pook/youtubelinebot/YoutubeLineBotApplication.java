357ff3d4b8755456c347db754ac9e6e91e1d6c39
Veeraya Pupatwibul <veeraya.pupat@gmail.com>
2017-11-08 21:22:59 +0800

    Making the app work with elastic beanstalk

--- src/main/java/com/pook/youtubelinebot/YoutubeLineBotApplication.java
+++ src/main/java/com/pook/youtubelinebot/YoutubeLineBotApplication.java
@@ -7,6 +7,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.attribute.FileAttribute;
+import java.nio.file.attribute.PosixFilePermission;
+import java.nio.file.attribute.PosixFilePermissions;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
@@ -15,7 +20,16 @@ public class YoutubeLineBotApplication {
 	static Path downloadedContentDir;
 	static BlockingQueue<YoutubeWorkUnit> youtubeWorkQueue;
 	public static void main(String[] args) throws IOException {
-		downloadedContentDir = Files.createTempDirectory("downloadedVideo");
+		Set<PosixFilePermission> perms = new HashSet<>();
+		perms.add(PosixFilePermission.OWNER_WRITE);
+		perms.add(PosixFilePermission.OWNER_READ);
+		perms.add(PosixFilePermission.GROUP_READ);
+		perms.add(PosixFilePermission.GROUP_WRITE);
+		perms.add(PosixFilePermission.OTHERS_READ);
+		perms.add(PosixFilePermission.OTHERS_WRITE);
+		FileAttribute<Set<PosixFilePermission>> fileAttributes = PosixFilePermissions.asFileAttribute(perms);
+
+		downloadedContentDir = Files.createTempDirectory("downloadedVideo", fileAttributes);
 		youtubeWorkQueue = new ArrayBlockingQueue<>(10);
 
 		ConfigurableApplicationContext context = SpringApplication.run(YoutubeLineBotApplication.class, args);
